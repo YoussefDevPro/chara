ee76b94312da227c472fb680807b4a1a75b62ab7
Youssef Lmouden <youssef.pro.coding@gmail.com>
2026-03-18 15:22:38 +0000

    added SurrealValue trait to bitmasks

--- src/core/models/marcos.rs
+++ src/core/models/marcos.rs
@@ -1,5 +1,3 @@
-use surrealdb::types::*;
-
 // TODO: add support for SurrealValue and work on the base service
 
 #[macro_export]
@@ -33,6 +31,35 @@ macro_rules! bitmask_serde {
         impl From<$ty> for i32 {
             fn from(val: $ty) -> i32 {
                 val.mask
+            }
+        }
+
+        impl SurrealValue for $ty {
+            fn kind_of() -> surrealdb::types::Kind {
+                surrealdb::types::Kind::Number
+            }
+
+            fn into_value(self) -> surrealdb::types::Value {
+                surrealdb::types::Value::Number(surrealdb::types::Number::Int(self.mask as i64))
+            }
+
+            fn from_value(value: surrealdb::types::Value) -> Result<Self, surrealdb::Error> {
+                match value {
+                    surrealdb::types::Value::Number(num) => match num {
+                        surrealdb::types::Number::Int(i) => Ok($ty { mask: i as i32 }),
+                        surrealdb::types::Number::Float(f) => Ok($ty { mask: f as i32 }),
+                        _ => Err(surrealdb::Error::thrown(
+                            "Unsupported number type for bitmask".to_string(),
+                        )),
+                    },
+                    _ => Err(surrealdb::Error::thrown(
+                        "Expected a numeric value for bitmask".to_string(),
+                    )),
+                }
+            }
+
+            fn is_value(value: &surrealdb::types::Value) -> bool {
+                matches!(value, surrealdb::types::Value::Number(_))
             }
         }
     };
