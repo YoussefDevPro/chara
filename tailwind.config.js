4e47d7da7c68d5bdff0be0c7a6207c0a412802ac
Youssef Lmouden <youssef.pro.coding@gmail.com>
2026-03-22 10:13:08 +0100

    added tailwind lessgoo

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/src/**/*.rs",
+    "./frontend/src/**/*.rs",
+    "./server/src/**/*.rs",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
