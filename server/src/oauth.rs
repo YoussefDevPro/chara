5b64a42569de0e294061d5eee66f51f3f12fcc48
Youssef Lmouden <youssef.pro.coding@gmail.com>
2026-03-31 09:32:48 +0100

    just for testing

--- server/src/oauth.rs
+++ server/src/oauth.rs
@@ -37,6 +37,7 @@ pub async fn oauth(
     })?;
     let tmp = service.create_session(ip, user_agent).await;
     let session_token = tmp.ok().ok_or(StatusCode::INTERNAL_SERVER_ERROR)?;
+    dbg!(&session_token);
 
     let cookie = Cookie::build(("session", session_token))
         .path("/")
@@ -46,6 +47,7 @@ pub async fn oauth(
         .build();
 
     let updated_jar = jar.add(cookie);
+    dbg!(&updated_jar);
 
     Ok((updated_jar, Redirect::to("/dashboard")))
 }
