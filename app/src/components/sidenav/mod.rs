5444816477b56221baa3094e2f7503cf0488ccfe
Youssef Lmouden <youssef.pro.coding@gmail.com>
2026-04-07 15:46:05 +0100

    made it sticky :3

--- app/src/components/sidenav/mod.rs
+++ app/src/components/sidenav/mod.rs
@@ -4,8 +4,8 @@ use leptos::prelude::*;
 #[component]
 pub fn SideNav() -> impl IntoView {
     view! {
-        <aside class="z-40 w-auto h-screen p-2 border-r">
-            <Annoyed class="size-12 text-black  text-[var(--foreground)] stroke-[var(foreground)] border p-2" />
+        <aside class="sticky top-0 z-40 w-auto h-screen p-2 border-r bg-background">
+            <Annoyed class="size-12 text-black text-[var(--foreground)] stroke-[var(foreground)] border p-2" />
         </aside>
     }
 }
